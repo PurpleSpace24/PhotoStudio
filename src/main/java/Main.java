12079d0c6aca5de9673b5f3f9f72f27867f41640
Yana Trayanova <73670359+PurpleSpace24@users.noreply.github.com>
2023-10-03 15:29:35 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,41 @@ import ENTITY.Order;
 import ENTITY.PhotoStudio;
 import ENTITY.Picture;
 
-import java.util.Date;
-import java.util.Scanner;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args){
+        LocalDate date = LocalDate.of(2023, 10, 1);
+        LocalDate date4 = LocalDate.of(2023, 10, 1);
+        LocalDate date2 = LocalDate.of(2023, 9, 29);
+        LocalDate date3 = LocalDate.of(2023, 9, 30);
 
         PhotoStudio photoStudio = new PhotoStudio();
 
-        Picture picture = new Picture(1, new Date(), "Alice", Order.FAST, 1024);
-        Picture picture2 = new Picture(3, new Date(), "John", Order.STANDARD, 512);
-        Picture picture3 = new Picture(2,new Date(), "Alex", Order.EXPRESS, 2048);
+        Picture picture = new Picture(1, date, "Alice", Order.FAST, 0.5);
+        Picture picture2 = new Picture(3, date2, "Yana", Order.STANDARD, 1);
+        Picture picture5 = new Picture(5, date2, "Ana", Order.STANDARD, 5.4);
+        Picture picture3 = new Picture(2,date3, "Vanesa", Order.EXPRESS, 3.4);
+        Picture picture4 = new Picture(4,date4, "Bob", Order.EXPRESS, 2.1);
 
         photoStudio.addPicture(picture);
         photoStudio.addPicture(picture2);
         photoStudio.addPicture(picture3);
+        photoStudio.addPicture(picture4);
+        photoStudio.addPicture(picture5);
 
+        System.out.println("All pictures: ");
         photoStudio.showPictures();
+        System.out.println();
+        System.out.println("Order by date then by owner name: ");
+        photoStudio.OrderByDateThenOwnerName();
+        System.out.println();
+        System.out.println("Display the photo that has the highest id: ");
+        photoStudio.biggerID();
+        //System.out.println("Return the collection of photos that are of the order type EXPRESS: ");
+        //photoStudio.pictureEXPRESS(); // not working. -> Why? -> I dont know :(
+        System.out.println("\nDisplay photos that are larger than 2MB in size: ");
+        photoStudio.pictureMoreThan2MB();
     }
 }
 
