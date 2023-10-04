7b73a20c55b61de2d70d4e35306428cb41bb2c61
Yana Trayanova <73670359+PurpleSpace24@users.noreply.github.com>
2023-10-04 14:47:18 +0300

    Update Order.java

--- src/main/java/ENTITY/Order.java
+++ src/main/java/ENTITY/Order.java
@@ -1,5 +1,19 @@
 package ENTITY;
 
 public enum Order {
-    STANDARD, FAST, EXPRESS
+    STANDARD(1), FAST(2), EXPRESS(3);
+
+    private Integer n;
+
+    Order(Integer n) {
+        this.n = n;
+    }
+
+    public Integer getN() {
+        return n;
+    }
+
+    public void setN(Integer n) {
+        this.n = n;
+    }
 }
