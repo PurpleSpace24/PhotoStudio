6267eefaaa12311da0dc1ba4dbabceb4f8384ff4
Yana Trayanova <73670359+PurpleSpace24@users.noreply.github.com>
2023-10-19 15:34:24 +0300

    Add files via upload

---
+++ src/main/java/ENTITY/Ffunc.java
@@ -0,0 +1,12 @@
+package Entity;
+
+public interface Ffunc {
+
+     void addSweets(Sweets sweets);
+     void displaySweets();
+     void displaySweetsByQuantityProduced();
+     void displaySweetsByProductionDateAndName();
+     void displayChocolateSweets();
+     void displaySweetsQuantityGreaterThan(int quantity);
+     void displaySweetsQuantityGreaterThan2();
+}
